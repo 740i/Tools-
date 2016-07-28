3577f5cf6a65ac9b364e54d37bede0031bb6879f
740i <740i@users.noreply.github.com>
2016-07-27 18:56:27 -0700

    Update wmienum.bat

--- wmienum.bat
+++ wmienum.bat
@@ -11,4 +11,3 @@ wmic startup get Caption,Command,Location,User /format:"%var%" >> out.html
 wmic PRODUCT get Description,InstallDate,InstallLocation,PackageCache,Vendor,Version /format:"%var%" >> out.html
 wmic os get name,version,InstallDate,LastBootUpTime,LocalDateTime,Manufacturer,RegisteredUser,ServicePackMajorVersion,Sys temDirectory /format:"%var%" >> out.html
 wmic Timezone get DaylightName,Description,StandardName /format:"%var%" >> out.html
-
